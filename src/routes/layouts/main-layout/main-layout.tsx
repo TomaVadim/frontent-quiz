9e3c570d77e5b53a5fce6a88ecc53f78a2096701
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-09-27 23:08:57 +0300

    chore: update imports

--- src/routes/layouts/main-layout/main-layout.tsx
+++ src/routes/layouts/main-layout/main-layout.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
-import { App } from "../../../App";
-import { Portal, SingIn } from "../../../features";
-import { Header } from "../../../components/header/header";
+import { App } from "@/App";
+import { Portal, SingIn } from "@/features";
+import { Header } from "@/components/header/header";
 
 export const MainLayout: FC = () => {
   return (
