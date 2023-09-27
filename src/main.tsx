d46e9bcd02f718f785632038000ee3bd6c3e960f
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2023-09-27 22:53:40 +0300

    feat: update app navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './stores/store.ts';
-import { App } from './App.tsx';
-import './assets/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { AppNavigation } from "./routes/app-navigation";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+import "./assets/index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <RouterProvider router={AppNavigation} />
+  </React.StrictMode>,
 );
