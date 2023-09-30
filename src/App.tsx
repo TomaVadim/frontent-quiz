1745507edd469da8300e243382304b0177ebd82e
Vadim Toma <https://github.com/TomaVadim>
2023-09-30 15:15:23 +0300

    feat: provider was wrapped in an error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Provider } from "react-redux";
 import { PropsWithChildren } from "react";
 
+import { ErrorBoundary } from "react-error-boundary";
+
+import { Fallback } from "./features/components/fallback/fallback";
+
 import store from "@/stores/redux/store";
 
 export const App = ({ children }: PropsWithChildren): JSX.Element => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <Provider store={store}>{children}</Provider>
+    </ErrorBoundary>
+  );
 };
