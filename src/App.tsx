727544c0553a22b176feb7bab57b7c992e407968
VadimToma <https://github.com/TomaVadim>
2023-10-01 18:31:10 +0300

    feat: wrap app component with theme and cache providers

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import { ErrorBoundary } from "react-error-boundary";
 
 import store from "@/stores/redux/store";
 import { errorFallbackRender } from "@/components/error-fallback-render/error-fallback-render";
+import { EmotionCacheProvider } from "@/components/emotion-cache-provider/emotion-cache-provider";
+import { ThemeProvider } from "@emotion/react";
+import { generateTheme } from "@/theme/theme";
 
 export const App = ({ children }: PropsWithChildren): JSX.Element => {
   return (
     <ErrorBoundary fallbackRender={errorFallbackRender}>
-      <Provider store={store}>{children}</Provider>
+      <EmotionCacheProvider>
+        <ThemeProvider theme={generateTheme()}>
+        <Provider store={store}>
+          {children}
+        </Provider>
+        </ThemeProvider>
+      </EmotionCacheProvider>
     </ErrorBoundary>
   );
 };
