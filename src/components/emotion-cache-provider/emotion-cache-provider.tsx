f1a30820f7f7e03b15051c25a06cd0643c84cfaa
VadimToma <https://github.com/TomaVadim>
2023-10-01 23:30:50 +0300

    feat: update emotion cache provider with create cache

--- src/components/emotion-cache-provider/emotion-cache-provider.tsx
+++ src/components/emotion-cache-provider/emotion-cache-provider.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from "react";
 
 import { CacheProvider } from "@emotion/react";
-import { cache } from "@emotion/css";
+import createCache from "@emotion/cache";
+
+const cache = createCache({
+  key: "custom",
+});
 
 export const EmotionCacheProvider = ({ children }: PropsWithChildren) => {
   return <CacheProvider value={cache}>{children}</CacheProvider>;
