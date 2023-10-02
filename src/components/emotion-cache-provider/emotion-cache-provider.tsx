36df639d4aa643b70245a811487e50f60f0eb0da
VadimToma <https://github.com/TomaVadim>
2023-10-02 10:44:09 +0300

    chore: change key in cache

--- src/components/emotion-cache-provider/emotion-cache-provider.tsx
+++ src/components/emotion-cache-provider/emotion-cache-provider.tsx
@@ -4,9 +4,8 @@ import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
 const cache = createCache({
-  key: "custom",
+  key: "mui",
 });
-
 export const EmotionCacheProvider = ({ children }: PropsWithChildren) => {
   return <CacheProvider value={cache}>{children}</CacheProvider>;
 };
