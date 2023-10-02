da379af44f73e04581caab173031567bee36f957
VadimToma <https://github.com/TomaVadim>
2023-10-02 10:10:31 +0300

    chore: format code in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Provider } from "react-redux";
 import { PropsWithChildren } from "react";
+import { ThemeProvider } from "@emotion/react";
 import { ErrorBoundary } from "react-error-boundary";
 
 import store from "@/stores/redux/store";
 import { errorFallbackRender } from "@/components/error-fallback-render/error-fallback-render";
 import { EmotionCacheProvider } from "@/components/emotion-cache-provider/emotion-cache-provider";
-import { ThemeProvider } from "@emotion/react";
 import { generateTheme } from "@/theme/theme";
 
 export const App = ({ children }: PropsWithChildren): JSX.Element => {
@@ -13,9 +13,7 @@ export const App = ({ children }: PropsWithChildren): JSX.Element => {
     <ErrorBoundary fallbackRender={errorFallbackRender}>
       <EmotionCacheProvider>
         <ThemeProvider theme={generateTheme()}>
-        <Provider store={store}>
-          {children}
-        </Provider>
+          <Provider store={store}>{children}</Provider>
         </ThemeProvider>
       </EmotionCacheProvider>
     </ErrorBoundary>
