995158a0447da267484f50f4b5b3cb619a471007
VadimToma <https://github.com/TomaVadim>
2023-10-02 13:22:26 +0300

    feat: create token servise

---
+++ src/shared/servises/token-servise.ts
@@ -0,0 +1,32 @@
+class TokenServise {
+  private accessTokenKey: string;
+  private refreshTokenKey: string;
+
+  constructor() {
+    this.accessTokenKey = "access_token";
+    this.refreshTokenKey = "refresh_token";
+  }
+
+  getAccessToken(): string | null {
+    return localStorage.getItem(this.accessTokenKey);
+  }
+
+  setAccessToken(token: string): void {
+    localStorage.setItem(this.accessTokenKey, token);
+  }
+
+  getRefreshToken(): string | null {
+    return localStorage.getItem(this.refreshTokenKey);
+  }
+
+  setRefreshToken(token: string): void {
+    localStorage.setItem(this.refreshTokenKey, token);
+  }
+
+  removeTokens(): void {
+    localStorage.removeItem(this.accessTokenKey);
+    localStorage.removeItem(this.refreshTokenKey);
+  }
+}
+
+export default new TokenServise();
