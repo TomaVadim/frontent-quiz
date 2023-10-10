9a96e3ea314268611f4e5c92f9a5f5634e224b66
VadimToma <https://github.com/TomaVadim>
2023-10-10 19:12:27 +0300

    feat: override default button styles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,12 +3,19 @@ import { Theme } from "@mui/material";
 
 import { palette } from "@/theme/palette";
 import { typography } from "@/theme/typography";
+import { buttonTheme } from "@/components/ui/button/button";
 
 export const generateTheme = (): Theme => {
   const theme = createTheme({
     palette,
     typography,
-    components: {},
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: buttonTheme,
+        },
+      },
+    },
   });
 
   return theme;
