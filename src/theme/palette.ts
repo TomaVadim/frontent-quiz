a767ff9a87e1795aff7607390b1f9b2b69666fdb
VadimToma <https://github.com/TomaVadim>
2023-10-10 16:17:21 +0300

    feat: specify all colors from the design

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,3 +1,22 @@
 import { PaletteOptions } from "@mui/material";
 
-export const palette: PaletteOptions = {};
+export const palette: PaletteOptions = {
+  primary: {
+    main: "#000000",
+    dark: "#383838",
+    light: "#3a3a3a",
+    contrastText: "#ffffff",
+  },
+  secondary: {
+    main: "#aaaaaa",
+    dark: "#373737",
+    light: "#d9d9d9",
+    contrastText: "#aaaaaaa",
+  },
+  info: {
+    main: "#383838",
+    dark: "#707070",
+    light: "#b6b6b6",
+    contrastText: "#4285f4",
+  },
+};
