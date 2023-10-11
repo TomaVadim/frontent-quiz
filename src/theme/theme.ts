fd289faaf64fcd8e374739e695230ba3c3774858
VadimToma <https://github.com/TomaVadim>
2023-10-11 14:04:42 +0300

    feat: update theme with overrides input mui styles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,14 +1,21 @@
 import { createTheme } from "@mui/material";
 import { Theme } from "@mui/material";
 
-import palette from "@/theme/palette";
-import typography from "@/theme/typography";
+import { palette } from "@/theme/palette";
+import { typography } from "@/theme/typography";
+import { inputTheme } from "@/components/ui/input/input-theme";
 
 export const generateTheme = (): Theme => {
   const theme = createTheme({
     palette,
     typography,
-    components: {},
+    components: {
+      MuiInput: {
+        styleOverrides: {
+          root: inputTheme,
+        },
+      },
+    },
   });
 
   return theme;
