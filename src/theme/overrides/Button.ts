2f900b1c9017fe28f51183c37f7edac3efa9cef9
VadimToma <https://github.com/TomaVadim>
2023-10-15 14:53:06 +0300

    fix: update button overrides

--- src/theme/overrides/Button.ts
+++ src/theme/overrides/Button.ts
@@ -1,14 +1,22 @@
-import { CSSProperties } from "@mui/material/styles/createMixins";
+import { Components, Theme } from "@mui/material";
 
-export const buttonTheme: CSSProperties = {
-  backgroundColor: "#3a3a3a",
-  boxShadow: "4px 6px 15px rgba(0, 0, 0, 0.06)",
-  borderRadius: "6px",
-  width: "100%",
-  maxHeight: "59px",
-  color: "#ffffff",
-  textTransform: "capitalize",
-  "&:hover": {
-    backgroundColor: "#707070",
-  },
+export const Button = (theme: Theme): Components => {
+  return {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#3a3a3a",
+          boxShadow: "4px 6px 15px rgba(0, 0, 0, 0.06)",
+          borderRadius: "6px",
+          width: "100%",
+          maxHeight: "59px",
+          color: "#ffffff",
+          textTransform: "capitalize",
+          "&:hover": {
+            backgroundColor: "#707070",
+          },
+        },
+      },
+    },
+  };
 };
